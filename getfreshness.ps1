f0d392367852abe24219c9fefc3bcf288b27ed50
Dina Berry <diberry@microsoft.com>
2019-02-11 16:32:02 -0800

    fixing errors

--- getfreshness.ps1
+++ getfreshness.ps1
@@ -1,6 +1,6 @@
 param([string]$azureContentDir)
 
-#$azureContentDir = "/Users/diberry/repos/azure-docs-pr/articles/cognitive-services/"
+#$azureContentDir = "c:\\Users\\diberry\\repos\\azure-docs-pr\\articles"
 Write-Host $azureContentDir
 
 # Freshness is 90, but subtract 10 because
@@ -24,55 +24,70 @@ ForEach($file in $files) {
   $content = get-content $file.FullName 
 
   $author = $content | select-string -pattern "ms.author: "
-  $author = $author.Line.ToString().Replace("ms.author: ","")
+  if($author) {
+    $author = $author.Line.ToString().Replace("ms.author: ","")
+  }
 
   $manager = $content | select-string -pattern "manager: "
-  $manager = $manager.Line.ToString().Replace("manager: ","")
+  if($manager){
+    $manager = $manager.Line.ToString().Replace("manager: ","")
+  }
   
   $topic = $content | select-string -pattern "topic: "
-  $topic = $topic.Line.ToString().Replace("topic: ","")
+  if($topic){
+    $topic = $topic.Line.ToString().Replace("topic: ","")
+  }
   
   $services = $content | select-string -pattern "services: "
-  $services = $services.Line.ToString().Replace("services: ","")
+  if($services){
+    $services = $services.Line.ToString().Replace("services: ","")
+  }
   
   $subservice = $content | select-string -pattern "ms.subservice: "
-  $subservice = $subservice.Line.ToString().Replace("ms.subservice: ","")
-
-  $date = $content | select-string -pattern "ms.date"
-  $date = [datetime]$date.ToString().Replace("ms.date: ","")
-  $date = Get-Date -Format d -Date $date
-
-
-
-  #Get the difference
-  $difference = New-TimeSpan -Start $date -End $now
-
-  if($difference.Days -gt $olderThan) {
-
-    Write-Host $file.Name
-
-    #Create a new object and set the date and filename as properties
-    $fileWithDate = new-object System.Object
-    #$fileWithDate | add-member -MemberType NoteProperty -name LastPublished -Value $date
-    # Figure out the 90 day stale date
-    $plus90 = Get-Date (Get-Date $date).AddDays(90) -Format d
-
-    #$fileWithDate | add-member -MemberType NoteProperty -name path -value $file.Path
-    #$fileWithDate | add-member -MemberType NoteProperty -name path -value $file.
-    $fileWithDate | add-member -MemberType NoteProperty -name 90Days -value $plus90
-    $fileWithDate | add-member -MemberType NoteProperty -name Author -Value $author
-    $fileWithDate | add-member -MemberType NoteProperty -name Manager -Value $manager
-    $fileWithDate | add-member -MemberType NoteProperty -name Topic -Value $topic
-    $fileWithDate | add-member -MemberType NoteProperty -name Services -Value $services
-    $fileWithDate | add-member -MemberType NoteProperty -name Subservice -Value $subservice
-    $fileWithDate | add-member -MemberType NoteProperty -name FileName -Value $file.Name
-    $fileWithDate | add-member -MemberType NoteProperty -name FolderName -Value $file.DirectoryName
-
-    #Add the object to the array
-    $filesWithDates+=$fileWithDate
+  if($subservice){
+    $subservice = $subservice.Line.ToString().Replace("ms.subservice: ","")
   }
+
+  [string]$difference=-1
+  [string]$plus90=""
+
+  $date = $content | select-string -pattern "ms.date:"
+  $pubdate = $date
+
+  if($date){
+    $date = [datetime]$date.ToString().Replace("ms.date: ","")
+    #$date = Get-Date -Format d -Date $date
+    $pubdate = $date
+
+    $difference = ($date - $now).Days
+    #$difference = $difference.Days
+    ##$newdiff = [datetime]::parseexact($date, 'dd-MMM-yy', $null)
+
+    $plus90 = Get-Date (Get-Date $date).AddDays(90) -Format d
+  }
+
+
+
+  $fileWithDate = new-object System.Object
+
+  $fileWithDate | add-member -MemberType NoteProperty -name DaysOld -value $difference
+  $fileWithDate | add-member -MemberType NoteProperty -name 90DaysOldDate -value $plus90
+  $fileWithDate | add-member -MemberType NoteProperty -name PubDate -value $pubdate
+  $fileWithDate | add-member -MemberType NoteProperty -name Author -Value $author
+  $fileWithDate | add-member -MemberType NoteProperty -name Manager -Value $manager
+  $fileWithDate | add-member -MemberType NoteProperty -name Topic -Value $topic
+  $fileWithDate | add-member -MemberType NoteProperty -name Services -Value $services
+  $fileWithDate | add-member -MemberType NoteProperty -name Subservice -Value $subservice
+  $fileWithDate | add-member -MemberType NoteProperty -name FileName -Value $file.Name
+  $fileWithDate | add-member -MemberType NoteProperty -name FolderName -Value $file.DirectoryName
+
+  #Add the object to the array
+  $filesWithDates+=$fileWithDate
+
 }
 Write-Host "done processing files"
-Write-Host $filesWithDates
 $filesWithDates | ConvertTo-Csv -NoTypeInformation | % {$_.Replace('"','')} | Out-File freshness.csv
 Write-Host "done creating freshness.csv"
+Write-Host $now
+$now = Get-Date
+Write-Host $now
