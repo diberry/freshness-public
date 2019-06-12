7ba2c370fbf25a474bc28130dc1e235e2af0204b
diberry <diberry@microsoft.com>
2019-06-12 09:04:27 -0700

    remove unused files

--- datetime.ps1
+++
@@ -1,8 +0,0 @@
-$now = Get-Date
-Write-Host $now
-
-$newnow = $now.ToString("yyyyMMdd")
-Write-Host $newnow
-
-$outfilename = $("freshness-{0}.csv" -f $(Get-Date -format 'yyyyMMdd'));
-Write-Host $outfilename
